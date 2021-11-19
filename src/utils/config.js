1ab2b802d9f1fdae4ec99477bd2ab2ddd689d705
chengfei <chengfei@xylink.com>
2021-11-19 15:53:38 +0800

    docs: config notes

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,7 +5,9 @@
  * @date  2020-01-17 12:04:01
  */
 export const THIRD = true;
-
+/**
+ * 默认小鱼服务地址，如有配置此类服务，需替换成自己的服务地址
+ */
 export const SERVER = {
   wssServer: "wss://cloudapi.xylink.com",
   httpServer: "https://cloudapi.xylink.com",
@@ -13,6 +15,10 @@ export const SERVER = {
   logServer: "https://log.xylink.com",
 };
 
+/**
+ * 以下参数需替换成自己企业的参数， 此参数可在企业管理平台注册，或者咨询项目对接人
+ * 
+ */
 export const ACCOUNT = {
   extId: "0142901e3d83e0a1e225ef92b8663fcaebda7242",
   clientId: "BR1e5cptJgyC1aFRtiXfmdg3",
